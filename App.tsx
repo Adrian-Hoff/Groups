b0fdc703a133794d7741604f8c581ebd23316277
hykepashi <adrian.hoff2005@gmail.com>
2023-05-02 17:46:37 -0300

    feat: expo google font and font loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
+// react
+import { ActivityIndicator } from "react-native";
+
+//themes
 import themes from "@themes/index";
+
+//screens
 import Groups from "./src/screens/groups";
+
+//styled
 import { ThemeProvider } from "styled-components";
 
+//expo-font
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={themes}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
