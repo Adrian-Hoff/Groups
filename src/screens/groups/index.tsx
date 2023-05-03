e56bf9720fae0babb0b2c25152338c2fc0a6e569
hykepashi <adrian.hoff2005@gmail.com>
2023-05-03 15:43:28 -0300

    feat: Groups/ add HeaderComponent into Groups

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,9 +1,18 @@
 //react
 import { StyleSheet, Text, View } from "react-native";
+
+//styles
 import * as _ from "./styles";
 
+//components
+import HeaderComponent from "@components/HeaderComponent";
+
 export default function Groups() {
-  return <_.Container></_.Container>;
+  return (
+    <_.Container>
+      <HeaderComponent />
+    </_.Container>
+  );
 }
 
 const styles = StyleSheet.create({});
