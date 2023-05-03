924f30c2d0325970f315fb9a9ff9b0ccf8bfefe1
hykepashi <adrian.hoff2005@gmail.com>
2023-05-03 16:54:02 -0300

    feat: Group/ add FlatList rendering GroupCardComponent from array state

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,5 +1,6 @@
 //react
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 //styles
 import * as _ from "./styles";
@@ -7,12 +8,27 @@ import * as _ from "./styles";
 //components
 import HeaderComponent from "@components/HeaderComponent";
 import Highlight from "@components/HighlightComponent";
+import GroupCardComponent from "@components/GroupCardComponent";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "rocket guys",
+    "rocket guys 2",
+    "rocket guys 3",
+  ]);
+
   return (
     <_.Container>
       <HeaderComponent />
       <Highlight title="Groups" subtitle="Enjoy with your group" />
+
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => {
+          return <GroupCardComponent title={item} />;
+        }}
+        keyExtractor={(item) => item}
+      />
     </_.Container>
   );
 }
