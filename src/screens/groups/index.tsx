00f9d7d1e92961574ffe2eadd128e7f0d1044bab
hykepashi <adrian.hoff2005@gmail.com>
2023-05-03 15:45:11 -0300

    feat: Groups/ add HighlightComponent into Groups

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -6,11 +6,13 @@ import * as _ from "./styles";
 
 //components
 import HeaderComponent from "@components/HeaderComponent";
+import Highlight from "@components/HighlightComponent";
 
 export default function Groups() {
   return (
     <_.Container>
       <HeaderComponent />
+      <Highlight title="Groups" subtitle="Enjoy with your group" />
     </_.Container>
   );
 }
