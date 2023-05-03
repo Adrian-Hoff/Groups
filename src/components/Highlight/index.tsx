f12a464490a12d24caf5418cadd29c8b74461ffa
hykepashi <adrian.hoff2005@gmail.com>
2023-05-02 21:50:54 -0300

    feat: * HighlightComponents

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,15 @@
+import * as _ from "./styles";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export default function Highlight({ title, subtitle }: Props) {
+  return (
+    <_.Container>
+      <_.Title>{title}</_.Title>
+      <_.Subtitle>{subtitle}</_.Subtitle>
+    </_.Container>
+  );
+}
