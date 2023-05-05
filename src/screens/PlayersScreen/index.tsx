f285b24de23162a76841a2993f97f52431f5327b
hykepashi <adrian.hoff2005@gmail.com>
2023-05-05 18:41:22 -0300

    feat: PlayersScreen/ add HighlightComponent into it

--- src/screens/PlayersScreen/index.tsx
+++ src/screens/PlayersScreen/index.tsx
@@ -1,10 +1,19 @@
-import HeaderComponent from "@components/HeaderComponent";
+//styles
 import { Container } from "./styles";
+
+//components
+import HighlightComponent from "@components/HighlightComponent";
+import HeaderComponent from "@components/HeaderComponent";
 
 export default function PlayerScreen() {
   return (
     <Container>
       <HeaderComponent showBackButton />
+
+      <HighlightComponent
+        title="Group Title"
+        subtitle="Add the gang to the Group"
+      />
     </Container>
   );
 }
