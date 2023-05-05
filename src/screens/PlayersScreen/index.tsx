3ba72f47a2de70ee87e0b640cc20089b9a9da1c8
hykepashi <adrian.hoff2005@gmail.com>
2023-05-05 18:45:04 -0300

    feat: PlayersScreen/ add form with InputComponent and InputComponent inside and inline

--- src/screens/PlayersScreen/index.tsx
+++ src/screens/PlayersScreen/index.tsx
@@ -1,9 +1,11 @@
 //styles
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 //components
 import HighlightComponent from "@components/HighlightComponent";
 import HeaderComponent from "@components/HeaderComponent";
+import InputComponent from "@components/InputComponent";
+import ButtonIconComponent from "@components/ButtonIconComponent";
 
 export default function PlayerScreen() {
   return (
@@ -14,6 +16,12 @@ export default function PlayerScreen() {
         title="Group Title"
         subtitle="Add the gang to the Group"
       />
+
+      <Form>
+        <InputComponent placeholder="Name" />
+
+        <ButtonIconComponent icon="plus" size={22} />
+      </Form>
     </Container>
   );
 }
