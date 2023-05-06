a25dc52378a5fab47a81abd5500292316ea910f1
hykepashi <adrian.hoff2005@gmail.com>
2023-05-06 12:33:46 -0300

    feat: HeaderComponent/ add handleGoToGrops function with useNavigation

--- src/components/HeaderComponent/index.tsx
+++ src/components/HeaderComponent/index.tsx
@@ -1,17 +1,28 @@
+//react-navigation
+import { useNavigation } from "@react-navigation/native";
+
+//styles
 import * as _ from "./styles";
 
+//images
 import logoImg from "@assets/logo.png";
-
 import Icon from "react-native-vector-icons/AntDesign";
+
 type Props = {
   showBackButton?: boolean;
 };
 
 export default function HeaderComponent({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoToGroups() {
+    navigation.navigate("groupsScreen");
+  }
+
   return (
     <_.Container>
       {showBackButton && (
-        <_.BackButton>
+        <_.BackButton onPress={handleGoToGroups}>
           <_.BackIcon name="left" />
         </_.BackButton>
       )}
