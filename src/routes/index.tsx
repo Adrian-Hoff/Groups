41a1bee7d5ba4f9327d3b32f54ab8e5ac2b54a21
hykepashi <adrian.hoff2005@gmail.com>
2023-05-06 14:14:49 -0300

    fix: Routes/ encapsulates NavigationContainer with a flex View with the same app backgroundColor. It solves the white flashing screen issue when the user navigates between the screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,19 @@
+// react
+import { View } from "react-native";
+
+//react-naviation
 import { NavigationContainer } from "@react-navigation/native";
+
+//routes
 import AppRoutes from "./app.routes";
+import themes from "@themes/index";
 
 export default function Routes() {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: themes.COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }
