b4f143440d5c0526009103cdb19856561a8c5198
hykepashi <adrian.hoff2005@gmail.com>
2023-05-06 11:24:53 -0300

    feat: AppRoutes(new)/ create stack navigator with 3 stacks: groupsScreen, newGroupScreen and playersScreen. missing NavigationContainer

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,19 @@
+//react
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Sreens
+import GroupsScreen from "@screens/GroupsScreen";
+import NewGroupScreen from "@screens/NewGroupScreen";
+import PlayersScreen from "@screens/PlayersScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="groupsScreen" component={GroupsScreen} />
+      <Stack.Screen name="newGroupScreen" component={NewGroupScreen} />
+      <Stack.Screen name="playersScreen" component={PlayersScreen} />
+    </Stack.Navigator>
+  );
+}
